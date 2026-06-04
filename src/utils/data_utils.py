64b6cb017bb8c2b95e4123c4b90784042427ef14
Yashuu05 <chillalyash2005@gmail.com>
2026-06-04 18:56:06 +0530

    created data helper functions

---
+++ src/utils/data_utils.py
@@ -0,0 +1,59 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+def load_data(df_path):
+    """
+    read csv file
+    input: csv file path
+    output: csv file or dataset
+    """
+    try:
+        df = pd.read_csv(df_path)
+        print(f"{df_path} load successfully")
+        return df
+    except Exception as e:
+        print(f"error: {e}")
+
+def split_dataset(randomState: int, testSize: float, X, y) -> tuple:
+    """
+    split dataset into training and testing with given ratio
+
+    Input: 
+    random state, testSize, X, y
+
+    Output: 
+    X_train, X_test, y_train, y_test  
+    """
+    try:
+        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=randomState, test_size=testSize, shuffle=True)
+        return X_train, X_test, y_train, y_test
+    except Exception as e:
+        print(f"error: {e}")
+
+def create_num_cat_list(df):
+    """
+    create the list of separate list of categorical and numerical features from the given datast
+
+    Input: 
+    df : dataset
+
+    Output: 
+    numerical_cols: columns with numerical features
+    categorical_cols: columns with categorical features 
+    """
+    numerical_cols = []
+    categorical_cols = []
+
+    try:
+        for cols in df.columns:
+            if df[cols].dtypes == "object":
+                categorical_cols.append(cols)
+            else:
+                numerical_cols.append(cols)
+
+        print("numerical cols: \n", numerical_cols)
+        print("\ncategorical cols: \n", categorical_cols)
+
+        return numerical_cols, categorical_cols
+
+    except Exception as e:
+        print(f"error: {e}")
